d7274c79e32504171548f5130aa3ec81c6360253
Sebastian Berglönn <sebastianberglonn@gmail.com>
2021-12-05 14:55:54 +0100

    day04 part 2

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -111,7 +111,11 @@ fn part_one(contents: String) -> u64 {
     let mut boards = board_numbers
         .chunks(5)
         .map(|chunk| {
-            let vec_of_strings = chunk.to_vec().iter().map(|&s| s.into()).collect::<Vec<String>>();
+            let vec_of_strings = chunk
+                .to_vec()
+                .iter()
+                .map(|&s| s.into())
+                .collect::<Vec<String>>();
             Bingo::new(vec_of_strings)
         })
         .collect::<Vec<Bingo>>();
@@ -122,7 +126,7 @@ fn part_one(contents: String) -> u64 {
             b.add_input(n);
             if b.are_we_winning() {
                 let sum: i32 = b.get_all_unused().iter().map(|n| *n as i32).sum();
-                return (sum * n as i32).try_into().unwrap()
+                return (sum * n as i32).try_into().unwrap();
             }
         }
     }
@@ -130,7 +134,45 @@ fn part_one(contents: String) -> u64 {
 }
 
 fn part_two(contents: String) -> u64 {
-    let splitted_contents = contents.split("\n").collect::<Vec<&str>>();
+    let mut list = contents.split("\n").collect::<Vec<&str>>();
+    let inputs_numbers = get_number_inputs(&list);
+    list.retain(|x| {
+        let q = x.trim();
+        !q.is_empty()
+    });
+    let board_numbers = list[1..].to_owned();
+    let mut boards = board_numbers
+        .chunks(5)
+        .map(|chunk| {
+            let vec_of_strings = chunk
+                .to_vec()
+                .iter()
+                .map(|&s| s.into())
+                .collect::<Vec<String>>();
+            Bingo::new(vec_of_strings)
+        })
+        .collect::<Vec<Bingo>>();
+    for s in inputs_numbers.iter() {
+        let n = s.parse::<u8>().unwrap();
+        let length_of_boards = boards.len();
+        let mut i: usize = 0;
+        while i < length_of_boards {
+            let mut shame = 1;
+            if let Some(b) = boards.get_mut(i) {
+                b.add_input(n);
+                if b.are_we_winning() {
+                    if length_of_boards == 1 {
+                        let sum: i32 = b.get_all_unused().iter().map(|number| *number as i32).sum();
+                        return (sum * n as i32).try_into().unwrap();
+                    } else {
+                        boards.remove(i);
+                        shame -= 1;
+                    }
+                }
+            }
+            i += shame;
+        }
+    }
     0
 }
 
@@ -331,4 +373,30 @@ fn test_should_win_and_calucate_correctly() {
     let expected_sum = 237;
     let unused: i32 = bingo.get_all_unused().iter().map(|n| *n as i32).sum();
     assert_eq!(unused, expected_sum);
+}
+
+#[test]
+fn test_part_two() {
+    let inputs = "7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+
+    22 13 17 11  0
+     8  2 23  4 24
+    21  9 14 16  7
+     6 10  3 18  5
+     1 12 20 15 19
+    
+     3 15  0  2 22
+     9 18 13 17  5
+    19  8  7 25 23
+    20 11 10 24  4
+    14 21 16 12  6
+    
+    14 21 17 24  4
+    10 16 15  9 19
+    18  8 23 26 20
+    22 11 13  6  5
+     2  0 12  3  7";
+
+    let q = part_two(inputs.into());
+    assert_eq!(q, 1924);
 }
